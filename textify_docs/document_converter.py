34e67d80237802b006573b7c9ed2e57698900d3f
BlcMed <boulaich.mohamed970@gmail.com>
2024-08-12 01:32:35 +0100

    refactor: document converter no longer take path as an attribute instead it is passed each time we call the convert to text function

--- textify_docs/document_converter.py
+++ textify_docs/document_converter.py
@@ -5,13 +5,15 @@ from .image_converter import ImageConverter
 from .xlsx_converter import XlsxConverter
 
 class DocumentConverter:
-    def __init__(self, file_path):
-        self.file_path = file_path
-        self.converter = self._select_converter(file_path)
+    def __init__(self):
+        self.converter = None
 
     def _select_converter(self, file_path):
         """
         Select the appropriate converter based on file extension.
+        
+        :param file_path: Path to the document file.
+        :return: An instance of the appropriate converter.
         """
         file_extension = os.path.splitext(file_path)[1].lower()
         
@@ -26,8 +28,12 @@ class DocumentConverter:
         else:
             raise ValueError("Unsupported file type")
 
-    def convert_to_text(self):
+    def convert_to_text(self, file_path):
         """
-        Convert any kond of document to plain text using the selected converter.
+        Convert the specified document to plain text using the appropriate converter.
+        
+        :param file_path: Path to the document file.
+        :return: A string containing the plain text extracted from the document.
         """
+        self.converter = self._select_converter(file_path)
         return self.converter.convert_to_text()
