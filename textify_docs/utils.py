f0c6e64b66c66e7c04d5c5674354a8480ec99b7d
BlcMed <boulaich.mohamed970@gmail.com>
2024-08-18 08:17:53 +0100

    feat: add utils file

---
+++ textify_docs/utils.py
@@ -0,0 +1,25 @@
+import torch
+
+def _box_cxcywh_to_xyxy(x):
+    x_c, y_c, w, h = x.unbind(-1)
+    b = [(x_c - 0.5 * w), (y_c - 0.5 * h), (x_c + 0.5 * w), (y_c + 0.5 * h)]
+    return torch.stack(b, dim=1)
+
+def _rescale_bboxes(out_bbox, size):
+    img_w, img_h = size
+    b = _box_cxcywh_to_xyxy(out_bbox)
+    b = b * torch.tensor([img_w, img_h, img_w, img_h], dtype=torch.float32)
+    return b
+
+def outputs_to_objects(outputs, img_size, id2label):
+    m = outputs.logits.softmax(-1).max(-1)
+    pred_labels = list(m.indices.detach().cpu().numpy())[0]
+    pred_scores = list(m.values.detach().cpu().numpy())[0]
+    pred_bboxes = outputs['pred_boxes'].detach().cpu()[0]
+    pred_bboxes = [elem.tolist() for elem in _rescale_bboxes(pred_bboxes, img_size)]
+    objects = []
+    for label, score, bbox in zip(pred_labels, pred_scores, pred_bboxes):
+        class_label = id2label[int(label)]
+        if not class_label == 'no object':
+            objects.append({'label': class_label, 'score': float(score), 'bbox': [float(elem) for elem in bbox]})
+    return objects
